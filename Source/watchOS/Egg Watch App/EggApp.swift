9bac68ff41c74847df3dea0506e593dddb51d738
iiKurt <38019652+iiKurt@users.noreply.github.com>
2023-05-02 15:34:38 +1200

    Dedicated watchOS port? 👀

---
+++ Source/watchOS/Egg Watch App/EggApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggApp.swift
+//  Egg Watch App
+//
+//  Created by Kurt on 2/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Egg_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
